dd9fdd032d01c83fde8fb84171cec1b73c83ef15
Muhammed Miah <miahsuleman14@gmail.com>
2020-09-18 13:57:10 +0100

    Deleted Commented Out Console Calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -52,67 +52,6 @@ namespace Calculator
             return answer;
         }
 
-        //public static void Calculator(string function)
-        //{
-        //    if (function == "Add")
-        //    {
-        //        Console.WriteLine("Please enter the numbers you would like to add?");
-        //        string entry = Console.ReadLine();
-        //        string[] values = entry.Split(',');
-        //        int number1 = int.Parse(values[0]);
-        //        int number2 = int.Parse(values[1]);
-        //        int answer = Add(number1, number2);
-        //        Console.WriteLine("The answer is " + answer);
-        //    }
-        //    if (function == "Subtract")
-        //    {
-        //        Console.WriteLine("Please enter the numbers you would like to subtract?");
-        //        string entry = Console.ReadLine();
-        //        string[] values = entry.Split(',');
-        //        int number1 = int.Parse(values[0]);
-        //        int number2 = int.Parse(values[1]);
-        //        int answer = Subtract(number1, number2);
-        //        Console.WriteLine("The answer is " + answer);
-        //    }
-        //    if (function == "Divide")
-        //    {
-        //        Console.WriteLine("Please enter the numbers you would like to divide?");
-        //        string entry = Console.ReadLine();
-        //        string[] values = entry.Split(',');
-        //        int number1 = int.Parse(values[0]);
-        //        int number2 = int.Parse(values[1]);
-        //        int answer = Divide(number1, number2);
-        //        Console.WriteLine("The answer is " + answer);
-        //    }
-        //    if (function == "Multiply")
-        //    {
-        //        Console.WriteLine("Please enter the numbers you would like to multiply?");
-        //        string entry = Console.ReadLine();
-        //        string[] values = entry.Split(',');
-        //        int number1 = int.Parse(values[0]);
-        //        int number2 = int.Parse(values[1]);
-        //        int answer = Multiply(number1, number2);
-        //        Console.WriteLine("The answer is " + answer);
-        //    }
-        //    if (function == "Modulus")
-        //    {
-        //        Console.WriteLine("Please enter the numbers you would like to find the Modulus for?");
-        //        string entry = Console.ReadLine();
-        //        string[] values = entry.Split(',');
-        //        int number1 = int.Parse(values[0]);
-        //        int number2 = int.Parse(values[1]);
-        //        int answer = Modulus(number1, number2);
-        //        Console.WriteLine("The answer is " + answer);
-        //    }
-        //    //else
-        //    //{
-        //    //    Console.WriteLine("Incorrect Function, Please try again.");
-        //    //    Console.WriteLine("Please Enter the function you would like to do");
-        //    //    var function1 = Console.ReadLine();
-        //    //    Calculator(function1);
-        //    //}
-        //}
-
         public static void Main(string[] args)
         {
             var result = DoThis(11, "numbers", out bool isLarge);
